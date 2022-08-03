ab81c4398d3810edb1e141b518322949e1e7df0c
carmona44 <dcacarmona44@gmail.com>
2022-08-03 18:30:54 +0200

    fix: dynamic api nfts server uri

--- src/cryptoavatars/constants.ts
+++ src/cryptoavatars/constants.ts
@@ -1,3 +1,3 @@
 export const OPENSEA_API_URL = 'https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50&collection=cryptoavatars&include_orders=false';
 
-export const API_NFTS_SERVER = 'http://localhost:3000';
+export const API_NFTS_SERVER = process.env.PROD_URI || 'http://localhost:3000';
